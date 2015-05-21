149899005a0b7f6e309de6a0b167f7720c6045e6
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2015-05-20 20:00:07 -0700

    scan chromedriver versions

--- run-continuous-integration.sh
+++ run-continuous-integration.sh
@@ -6,5 +6,15 @@ set -x
 git log --oneline -n 5
 sudo ./install-build-deps.sh
 sudo start xvfb || true
-sudo ant test -Dtest.shuffle=1 -Dtest.repeat=1
-sudo ant package
+
+for i in 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+
+rm *.zip || true
+wget https://chromedriver.storage.googleapis.com/2.$i/chromedriver_linux64.zip
+chmod +r *.zip
+unzip -o -d buildtools *.zip
+chmod +x buildtools/chromedriver
+
+sudo ant runtest -Dtest.shuffle=1 -Dtest.repeat=1 || true
+
+done
