d3e8ff5338612128a508bc5aad2e4f7069a1e8b0
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2015-05-20 20:12:29 -0700

    run chrome first

--- run-continuous-integration.sh
+++ run-continuous-integration.sh
@@ -7,10 +7,16 @@ git log --oneline -n 5
 sudo ./install-build-deps.sh
 sudo start xvfb || true
 
+ls -al ~/.config || true
+
+timeout 10 xvfb-run google-chrome http://google.com
+
+ls -al ~/.config || true
+
 for i in 3 4 5 6 7 8 9 10 11 12 13 14 15; do
 
 rm *.zip || true
-wget https://chromedriver.storage.googleapis.com/2.$i/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/2.$i/chromedriver_linux64.zip > /dev/null 2>&1
 chmod +r *.zip
 unzip -o -d buildtools *.zip
 chmod +x buildtools/chromedriver
