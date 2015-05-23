10ada6e80ca78679fb1d6f7e3f155c069d2ea7f7
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2015-05-22 21:13:23 -0700

    cmd

--- run-continuous-integration.sh
+++ run-continuous-integration.sh
@@ -3,8 +3,6 @@
 set -e
 set -x
 
-git log --oneline -n 5
-sudo ./install-build-deps.sh
-sudo start xvfb || true
-sudo ant test -Dtest.shuffle=1 -Dtest.repeat=10 -Dtest.filter='-*.ParsedUrlTest.*:*PagePatternTest.testIsPagePatternValid:*PagePatternTest.testIsPagingUrl'
-sudo ant package
+sudo apt-get install vagrant
+vagrant up
+vagrant ssh -c 'cd /vagrant && yes | ./vagrant.sh'
